b45ce0ba88e739055521df24a2e9252d12267efd
AlexBelov <mail.alexbelov@gmail.com>
2026-07-07 12:14:05 +0300

    added submission

---
+++ ReactHomework4/src/config/footer.js
@@ -0,0 +1,37 @@
+export const FOOTER_COLUMNS = [
+  {
+    title: 'Company',
+    links: [
+      { label: 'Home', href: '/' },
+      { label: 'Order', href: '/order' },
+      { label: 'FAQ', href: '/faq' },
+      { label: 'Contact', href: 'mailto:hello@website.com?subject=Hi' },
+    ],
+  },
+  {
+    title: 'Template',
+    links: [
+      { label: 'Style Guide', href: 'https://www.google.com/', external: true },
+      { label: 'Changelog', href: 'https://www.google.com/', external: true },
+      { label: 'Licence', href: 'https://www.google.com/', external: true },
+      { label: 'Webflow University', href: 'https://www.google.com/', external: true },
+    ],
+  },
+  {
+    title: 'Flowbase',
+    links: [
+      { label: 'More Cloneables', href: 'https://www.flowbase.co', external: true },
+    ],
+  },
+];
+
+export const FOOTER_SOCIALS = [
+  { label: 'Instagram', href: '#', icon: 'instagram' },
+  { label: 'Twitter', href: '#', icon: 'twitter' },
+  { label: 'YouTube', href: '#', icon: 'youtube' },
+];
+
+export const FOOTER_LEGAL = {
+  builtBy: { label: 'Flowbase', href: 'https://www.flowbase.co' },
+  poweredBy: { label: 'Webflow', href: 'http://webflow.com/' },
+};
